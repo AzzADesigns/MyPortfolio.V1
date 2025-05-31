32b9a57f55e4bb1a20c2c125fb3db0a59f64d59f
AzzADesigns <azariiielll@gmail.com>
2025-05-30 22:58:43 -0300

    alienando padding de navbar mobile

--- src/app/components/Header/themeAndTraductor/Them_Trans.tsx
+++ src/app/components/Header/themeAndTraductor/Them_Trans.tsx
@@ -7,7 +7,7 @@ import { Translator } from './Translator'
 export const Them_Trans = () => {
     return (
         <div className='fixed top-0 left-0  right-0 z-50 md:relative md:top-auto md:left-auto md:right-0 md:w-auto lg:w-[18%] w-full'>
-            <Card extraClass={"w-full  h-14 rounded-t-none md:rounded-t-3xl md:h-60 px-5 md:px-0 justify-between md:justify-center flex transition-all duration-300 flex-row md:flex-col items-center md:gap-5"}>
+            <Card extraClass={"w-full  h-14 rounded-t-none md:rounded-t-3xl md:h-60 px-4 md:px-0 justify-between md:justify-center flex transition-all duration-300 flex-row md:flex-col items-center md:gap-5"}>
                 <Translator/>
                 <Theme/>
             </Card>
