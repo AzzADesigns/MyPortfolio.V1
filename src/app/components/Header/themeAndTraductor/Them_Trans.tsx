2b7f47706791ebf3abe76224fff122c30ccca9e9
AzzADesigns <azariiielll@gmail.com>
2025-06-07 23:31:43 -0300

    reparando problemas de maquetacion

--- src/app/components/Header/themeAndTraductor/Them_Trans.tsx
+++ src/app/components/Header/themeAndTraductor/Them_Trans.tsx
@@ -6,7 +6,7 @@ import { Translator } from "./Translator";
 
 export const Them_Trans = () => {
     return (
-        <div className="fixed  top-0 left-0 right-0 md:right-auto z-50  md:top-auto md:left-auto  md:w-50 lg:w-72  w-full">
+        <div className="fixed w-full 2xl:right-100 xl:w-70 xl:top-12">
             <Card
                 extraClass={
                     "w-full  h-14 rounded-t-none md:rounded-t-3xl md:h-60 px-4 md:px-0 justify-between md:justify-center flex transition-all duration-300 flex-row md:flex-col items-center md:gap-5"
