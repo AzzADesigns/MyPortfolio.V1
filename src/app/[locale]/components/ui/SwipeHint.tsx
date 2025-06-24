5c4db834897cf4194980a1415b9060c00a127e24
AzzADesigns <azariiielll@gmail.com>
2025-06-24 01:44:58 -0300

    ultimo detalle

--- src/app/[locale]/components/ui/SwipeHint.tsx
+++ src/app/[locale]/components/ui/SwipeHint.tsx
@@ -4,9 +4,11 @@ import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import React, { useRef } from 'react';
 import { FaHandPointer } from 'react-icons/fa';
+import { useTranslations } from 'next-intl';
 
 export const SwipeHint: React.FC = () => {
     const handRef = useRef(null);
+    const t = useTranslations('textsPage');
 
     useGSAP(() => {
         gsap.to(handRef.current, {
@@ -23,7 +25,7 @@ export const SwipeHint: React.FC = () => {
             <div ref={handRef} className="text-2xl ml-12">
                 <FaHandPointer className='text-white/90'/>
             </div>
-            <span className="text-xs mt-1 ">Desliza para ver más</span>
+            <span className="text-xs mt-1 ">{t("textSwipe")}</span>
         </div>
     );
 };
