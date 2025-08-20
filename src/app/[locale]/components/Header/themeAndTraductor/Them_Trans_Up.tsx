3c6bf5ba284415a7a3685c52adf559a7582d4545
AzzADesigns <azariiielll@gmail.com>
2025-08-20 20:14:09 -0300

    fix: fixed problem in screens

--- src/app/[locale]/components/Header/themeAndTraductor/Them_Trans_Up.tsx
+++ src/app/[locale]/components/Header/themeAndTraductor/Them_Trans_Up.tsx
@@ -32,7 +32,7 @@ export const Them_Trans = () => {
         <div
             ref={containerRef}
             style={{ opacity: 0 }}
-            className="w-full xl:w-73 new2xl:w-73 3xl:w-83 border-b-2 xl:border-b-0 border-buttonColor backdrop-blur-sm bg-background/20 xl:bg-foreground rounded-t-none xl:rounded-t-3xl xl:rounded-3xl h-15 xl:h-70 p-10 px-4 md:px-5 justify-between flex transition-all duration-300 flex-row xl:flex-col items-center md:gap-5"
+            className="w-full xl:w-73 new2xl:w-73 3xl:w-93 border-b-2 xl:border-b-0 border-buttonColor backdrop-blur-sm bg-background/20 xl:bg-foreground rounded-t-none xl:rounded-t-3xl xl:rounded-3xl h-15 xl:h-70 p-10 px-4 md:px-5 justify-between flex transition-all duration-300 flex-row xl:flex-col items-center md:gap-5"
         >
             <Translator />
             <div className="h-1 hidden xl:flex w-[95%] rounded-full bg-background"></div>
